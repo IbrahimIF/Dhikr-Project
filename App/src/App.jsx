819c4aa685a008113152a66aaec1cafc79cca345
IbrahimIF <ibrahimfarrah30@gmail.com>
2026-03-04 23:46:59 +0000

    fix: box sizing and layout issues with arabic in dropdown

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import './styles/layout.css';
 import archImage from './assets/Arch.png';
 import WindowTitleBar from './WindowBar/WindowTitleBar';
-import Dropdown from './components/Dropdown';
+import Dropdown from './components/DropDown';
 import PrayerCard from './components/PrayerCard';
 import TimezoneSelector from './components/TimezoneSelector';
 import { usePrayerLogic } from './hooks/usePrayerLogic';
@@ -44,7 +44,8 @@ function App() {
         {dhikrs.map((d) => (
           <li key={d.id} className="listitem">
             <div className="article">
-              <strong>{d.title}</strong>: {d.arabic}
+              <div className="article-title">{d.title}</div>
+              <div className="article-arabic">{d.arabic}</div>
             </div>
           </li>
         ))}
